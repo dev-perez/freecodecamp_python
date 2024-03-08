9853ccae7af2402655591e28bfd00e24ce55932a
dev-perez <alyne.kmp@gmail.com>
2024-03-08 17:12:19 -0300

    resolução dos primeiros exercícios.

---
+++ 01.py
@@ -0,0 +1,14 @@
+# Passo 1
+# Variáveis são uma parte essencial do Python
+# e de qualquer linguagem de programação.
+# Uma variável é um nome que faz referência ou aponta para um objeto.
+# Você pode declarar uma variável escrevendo o nome da variável
+# no lado esquerdo do operador de atribuição = e especificando o valor
+# a ser atribuído a essa variável no lado direito do operador de atribuição:
+
+# nome_da_variável = valor
+
+# Crie uma variável chamada 'number' e atribua o valor 5 à sua nova variável.
+
+
+number = 5
---
+++ 02.py
@@ -0,0 +1,18 @@
+# Passo 2
+# Variáveis podem armazenar valores de diferentes tipos de dados.
+# Você acabou de atribuir um valor inteiro,
+# mas se deseja representar algum texto,
+# precisa atribuir uma string. Strings são sequências de caracteres
+# entre aspas simples ou duplas, mas você não pode começar uma string
+# com uma aspa simples e terminá-la com uma aspa dupla, ou vice-versa:
+
+# string_1 = "Eu sou uma string"
+# string_2 = 'Eu também sou uma string'
+# string_3 = 'Isso não é válido"
+
+# Apague sua variável 'number' e seu valor. Em seguida, declare
+# outra variável chamada 'text'
+# e atribua a string 'Hello World' a essa variável."
+
+
+text = "Hello World"
