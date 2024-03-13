63b5ed8c7c828fd2e11bec87d7547a5a883cd985
dev-perez <alyne.kmp@gmail.com>
2024-03-13 16:39:30 -0300

    feat: adding exercises 11 and 12

---
+++ 11.py
@@ -0,0 +1,9 @@
+# Passo 11
+# Modifique sua chamada de print(shift) 
+# para imprimir o tipo de dados da sua variável shift.
+
+
+text = 'Hello World'
+print(type(text))
+shift = 3
+print(type(shift))
---
+++ 12.py
@@ -0,0 +1,24 @@
+# Passo 12
+# Aspectos-chave na nomenclatura de variáveis em Python são:
+
+# - Algumas palavras são palavras-chave reservadas 
+# (por exemplo, for, while, True). 
+# Elas têm um significado especial em Python, 
+# então você não pode usá-las como nomes de variáveis.
+# - Nomes de variáveis não podem começar com um número, 
+# e só podem conter caracteres alfanuméricos ou sublinhados.
+# - Nomes de variáveis são sensíveis a maiúsculas e minúsculas, 
+# ou seja, my_var é diferente de my_Var e MY_VAR.
+# - Finalmente, é uma convenção comum escrever nomes de variáveis 
+# usando snake_case, onde cada espaço é substituído por 
+# um caractere de sublinhado e 
+# as palavras são escritas em letras minúsculas.
+
+# Remova ambas as chamadas para print() 
+# e declare outra variável chamada alphabet. 
+# Atribua a string 'abcdefghijklmnopqrstuvwxyz' a esta variável.
+
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
